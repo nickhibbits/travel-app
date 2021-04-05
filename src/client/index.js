57421dca43e95fc32b77219f12e511a1b90b4e49
nicholas-hibbits <nicholashibbits@gmail.com>
2021-04-04 18:47:08 -0700

    Feat: create function to get coordinates from geonames

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,6 @@ import './styles/style.scss'
 // Export statements for js
 export { performAction }
 
-
 // get reference to the button elemet
 const button = document.getElementById("generate");
 // Add submit event listener on this form
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,15 +1,11 @@
 /* Global Variables */
 let baseURL = "api.geonames.org/search?username=nickhibbits&name=";
-const apiKey = "&appid=cf671a0e6004b23e0fa755be924ec763&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+1 + "." + d.getDate() + "." + d.getFullYear();
 
-// Do the following when the generate button is clicked
-document.getElementById("generate").addEventListener("click", performAction);
-
-// GET weather info from API, then POST that info, along with user's entry, to the UI
+// IN PROGRESS, GET coordinates of destination, returns weather and a picture
 function performAction(e) {
     let dest = document.getElementById("zip").value;
     // let feelings = document.getElementById("feelings").value;
@@ -23,7 +19,7 @@ function performAction(e) {
 // Export performAction function for webpack entry
 export { performAction }
 
-// Access the OpenWeather API to get local weather data based on the zip code
+// Access the Geonames API to get coordinates of destination
 const getLocation = async (baseURL, loc) => {
     const res = await fetch(baseURL + loc);
     try {
@@ -35,35 +31,35 @@ const getLocation = async (baseURL, loc) => {
     }
 };
 
-// POST request to grab data returned from OpenWeather API
-const postData = async (url = "", newInfo = {}) => {
-    const response = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        // Body data type must match "Content-Type" header
-        body: JSON.stringify(newInfo),
-    });
-    try {
-        const newData = await response.json();
-        return newData;
-    } catch (error) {
-        console.log("error", error);
-    }
-};
-
-// Update UI
-const update = async () => {
-    const request = await fetch("/updatePage");
-    try {
-        const allData = await request.json();
-        // console.log(allData);
-        document.getElementById("temp").innerHTML = allData["entry"].temperature;
-        document.getElementById("date").innerHTML = allData["entry"].date;
-        document.getElementById("content").innerHTML = allData["entry"].entry;
-    } catch (error) {
-        console.log("error", error);
-    }
-};
+// // POST request to grab data returned from OpenWeather API
+// const postData = async (url = "", newInfo = {}) => {
+//     const response = await fetch(url, {
+//         method: "POST",
+//         credentials: "same-origin",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         // Body data type must match "Content-Type" header
+//         body: JSON.stringify(newInfo),
+//     });
+//     try {
+//         const newData = await response.json();
+//         return newData;
+//     } catch (error) {
+//         console.log("error", error);
+//     }
+// };
+//
+// // Update UI
+// const update = async () => {
+//     const request = await fetch("/updatePage");
+//     try {
+//         const allData = await request.json();
+//         // console.log(allData);
+//         document.getElementById("temp").innerHTML = allData["entry"].temperature;
+//         document.getElementById("date").innerHTML = allData["entry"].date;
+//         document.getElementById("content").innerHTML = allData["entry"].entry;
+//     } catch (error) {
+//         console.log("error", error);
+//     }
+// };
