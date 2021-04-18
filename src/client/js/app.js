4ee43a60b769f1a97ff4b9042a5aca44c99c30cc
nicholas-hibbits <nicholashibbits@gmail.com>
2021-04-18 12:38:13 -0700

    Feature: Add function to compare dates

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,7 +3,7 @@ let baseURL = "http://api.geonames.org/searchJSON?username=nickhibbits&maxRows=1
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+1 + "." + d.getDate() + "." + d.getFullYear();
+// let newDate = d.getMonth()+1 + "." + d.getDate() + "." + d.getFullYear();
 
 // IN PROGRESS: performAction to GET coordinates of destination, compare deprture date with current date, return date-dependent weather with picture
 function performAction(e) {
@@ -13,9 +13,15 @@ function performAction(e) {
     // console.log(d);
     getLocation(baseURL, dest)
     .then(function(data) {
-      console.log(data.geonames[0]);
+      console.log(data.geonames[0].lat);
+      console.log(data.geonames[0].lng);
+      console.log(data.geonames[0].countryName);
+      console.log(date);
+      console.log(d);
 
       // TODO: conditional statement to use different routes as first arg in postWeather(), to call different Weatherbit url's on server-side, depending on departure date
+
+      dateCompare(date, d)
 
         // postWeather("/current", { country:data.geonames[0], latitude: data.geonames[0].lat, longitude:data.geonames[0].lng});
         // update();
@@ -35,6 +41,20 @@ const getLocation = async (baseURL, loc) => {
         console.log("error", error);
     }
 };
+
+// Compare dates to get either current or future weather from Weatherbit
+function dateCompare(d1, d2){
+    const date1 = new Date(d1);
+    const date2 = new Date(d2);
+
+    if(date1 > date2){
+        console.log(`${d1} is later than ${d2}`)
+    } else if(date1 < date2){
+        console.log(`${d2} is later than ${d1}`)
+    } else{
+        console.log(`Both dates are equal`)
+    }
+}
 
 // POST request to display data returned from Weatherbit API
 const postWeather = async (url = "", newInfo = {} ) => {
