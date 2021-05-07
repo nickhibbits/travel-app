9b236f49af726275e30badc88426ddd6fe321fee
nicholas-hibbits <nicholashibbits@gmail.com>
2021-05-06 17:20:40 -0700

    Feature: Set min input date to current day, via JS

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -8,9 +8,9 @@ function performAction(e) {
     let dest = document.getElementById("dest").value;
     getLocation(baseURL, dest)
     .then(function(data) {
-      // console.log(data.geonames[0].lat);
-      // console.log(data.geonames[0].lng);
-      // console.log(data.geonames[0].countryName);
+      console.log(data.geonames[0].lat);
+      console.log(data.geonames[0].lng);
+      console.log(data.geonames[0].countryName);
       dateCompare(data)
       // .then(update())
     });
@@ -29,18 +29,51 @@ const getLocation = async (baseURL, loc) => {
 
 // Compare dates to get either current or future weather from Weatherbit
 // TODO: 1. Update conditional to determine if the trip date is within 7 days of the current date... 2. Determine if getWeather needs to be POST or GET request
-function dateCompare() {
-    var userDate = moment(new Date(document.getElementById("depart").value));
-    var cutoff = moment().add(7, "days"); // how to set the end 7 days from now?
-    var duration = moment.duration(userDate.diff(cutoff));
-    var days = duration.asDays();
-        if (days <= 7) {
-          postWeather("/current", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
-          console.log('input date is within 7 days of current date');
-        } else {
-          postWeather("/future", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
-          console.log('input date is more than 7 days away from current date');
-        }
+// function dateCompare(data) {
+//     var userDate = moment(new Date(document.getElementById("depart").value));
+//     console.log(userDate);
+//     var cutoff = moment().add(7, "days"); // how to set the end 7 days from now?
+//     // console.log(cuttoff);
+//     var duration = moment.duration(userDate.diff(cutoff));
+//     var days = duration.asDays();
+//         if (days <= 7) {
+//           // postWeather("/current", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
+//           console.log('input date is within 7 days of current date');
+//         } else {
+//           // postWeather("/future", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
+//           console.log('input date is more than 7 days away from current date');
+//         }
+// }
+
+// Attempt 2
+function dateCompare(data) {
+  // set minimum date for current day
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1;
+    let yyyy = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    }
+    if(mm<10){
+      mm='0'+mm
+    }
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("depart").setAttribute("min", today);
+
+  // determine if user date is within 7 days of current date
+    var userDate = new Date(document.getElementById("depart").value);
+    console.log(userDate);
+    // var cutoff = moment().add(7, "days"); // how to set the end 7 days from now?
+    // var duration = moment.duration(userDate.diff(cutoff));
+    // var days = duration.asDays();
+    //     if (days <= 7) {
+    //       // postWeather("/current", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
+    //       console.log('input date is within 7 days of current date');
+    //     } else {
+    //       // postWeather("/future", {country:data.geonames[0], latitude:data.geonames[0].lat, longitude:data.geonames[0].lng});
+    //       console.log('input date is more than 7 days away from current date');
+    //     }
 }
 
 // POST request to display data returned from Weatherbit API
